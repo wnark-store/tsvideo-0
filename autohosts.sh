82982a3a61827b6a432ef98b3cc7ee0597646b85
wnark-store <65661906+wnark-store@users.noreply.github.com>
2020-08-10 21:19:32 +0800

    Update autohosts.sh

--- autohosts.sh
+++ autohosts.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-if [ ! -f /etc/hostsbk ]; then
-cp /etc/hosts /etc/hostsbk
+dockerhosts="/etc/hosts"
+dockerhostsbk="/etc/hostsbk"
+if [ ! -f "$dockerhostsbk" ]; then
+cp "$dockerhosts" "$dockerhostsbk"
 fi
 echo -e "本项目由：寒夜方舟（wnark.com）创建及维护.\n本项目执行(CC BY-NC 4.0).\n请选择加速还是恢复备份.\n（已自动备份）"
 echo " 1: 加速"
 echo " 2: 恢复"
 read -p "请输入1|2。输入其他数字将会跳出脚本:"  idx
-if [ "1" == $idx ];then
-  cp /etc/hostsbk  /etc/hosts
+if [ '1' == $idx ];then
+  cp $dockerhostsbk $dockerhosts
   echo "129.146.46.7 github-cloud.s3.amazonaws.com" >> "$dockerhosts"
   echo "129.146.46.7 github.com" >> "$dockerhosts"
   echo "129.146.46.7 registry.cnpmjs.org" >> "$dockerhosts"
   echo "129.146.46.7 registry.npmjs.com" >> "$dockerhosts"
   echo "129.146.46.7 r.cnpmjs.org" >> "$dockerhosts"
   echo "129.146.46.7 raw.githubusercontent.com" >> "$dockerhosts" 
-elif [ "2" == $idx ];then
-  cp /etc/hostsbk  /etc/hosts
+elif [ '2' == $idx ];then
+  cp $dockerhostsbk $dockerhosts
 else
   echo "退出"
 fi
